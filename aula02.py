a8d5de65503af325095710c07a4dd5ca6a855c2b
PatriciaGavloski <patricia.navaski.g@gmail.com>
2025-12-17 14:28:24 -0300

    main

--- aula02.py
+++ aula02.py
@@ -8,9 +8,17 @@ print(bmi)
 print(round(bmi))
 
 print(round(bmi, 2))
-"""
+
 
 score = 0
 
 score += 1
-print(score)
+print("Yor score is " + str(score))
+
+"""
+
+score = 0
+height = 1.8
+is_winning = True
+
+print(f"Your score is = {score}, your height is {height} You are winnis is {is_winning}")
