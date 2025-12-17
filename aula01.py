453679f2cd5f6aa2eaec8b953754fac5e99d91e2
PatriciaGavloski <patricia.navaski.g@gmail.com>
2025-12-17 09:10:02 -0300

    Aulas do curso da udemy python bootcamp

---
+++ aula01.py
@@ -0,0 +1,47 @@
+"""print("Hello, World!\nHello Word")
+print("Hello" + " "  + "Angela")
+
+
+#def Print(a, b, c):
+    print("A =", a)
+    print("B =", b)
+    print("C =", c)
+
+print(1, 2, 3)
+
+
+def soma(x, y):
+    print(x + y)    
+
+
+soma(5, 10)
+"""
+"""
+print ("Hello" + " " + input ("What is your name? ") + "!")
+
+"""
+"""
+name = input("What is your name? ")
+#print (len("Hello " + name))
+print ("Your name has " + str(len(name)) + " characters")
+
+"""
+"""
+username = input("What is your username? ")
+length = len(username)
+print(length)
+
+"""
+"""
+user_name = "Patricia"
+length = len(user_name)
+print(length)
+
+"""
+#Project - Band name
+
+print("Seja bem-vindo ao gerador de nome de banda \n")
+cidade = input ("Qual é a cidade que você nasceu? \n") #city
+bichinho = input("Qual é o nome do seu bichinho de estimação? \n") #pet's name
+
+print("O nome da sua banda é " + bichinho + " de " + cidade)
---
+++ aula02.py
@@ -0,0 +1,16 @@
+"""height = 1.65
+weight = 84
+
+bmi = weight / (height)**2
+
+print(bmi)
+
+print(round(bmi))
+
+print(round(bmi, 2))
+"""
+
+score = 0
+
+score += 1
+print(score)
